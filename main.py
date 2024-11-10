79f2b23926d15e87c3f62f6e609b41b496cd7c21
Văn Dũng <131602164+XDgamerVN@users.noreply.github.com>
2024-11-10 23:31:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from interface import main_menu
-from decryption import *
-import os
-import pygame
+from interface import *
+
 
 pygame.init()
+
 
 # Hàm để lấy đường dẫn tài nguyên khi đóng gói bằng PyInstaller
 def resource_path(relative_path):
@@ -11,9 +10,18 @@ def resource_path(relative_path):
     base_path = os.path.abspath(".")  # Khi chạy trực tiếp từ mã nguồn
     return os.path.join(base_path, relative_path)
 
+
 icon = decrypt_image('logo', (256, 256))
 pygame.display.set_icon(icon)
 pygame.display.set_caption('Chess_Python by Nguyen Le Van Dung')
 
+
+def main():
+    try:
+        main_menu()
+    except Exception as e:
+        pass
+
+
 if __name__ == '__main__':
-    main_menu()
+    main()
